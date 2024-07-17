dd6f624d7bee2d1950584327d62e776cef33607d
Jean Azevedo <jean.azevedo1533@gmail.com>
2024-07-17 20:03:03 -0300

    desafio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let valor1 = prompt("Digite o nome")
+let valor2 =parseInt(prompt("Digite sua idade"))
+  
+  if(valor2>=18){
+    alert(`Bem vindo,\n${valor1} voce é maior de idade`)
+}else{
+    alert(`Nao autorizado`)
+}
+//
+
